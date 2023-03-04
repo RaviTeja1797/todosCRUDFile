b0d3dc97f066ad6d267277ae121f009bcf500d51
Ravi Teja <ravi_teja_1797@yahoo.com>
2023-03-04 07:30:41 +0000

    Recieved Database Connection object and Initialized the server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {open} = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const expressAppInstance = express();
+expressAppInstance.use(express.json());
+
+const dataBaseAddressPath = path.join(__dirname, "todoApplication.db");
+let databaseConnectionObject = null;
+
+const initializeDBAndServerAtPort3000 = async()=>{
+    try{
+        databaseConnectionObject = await open({
+            filename: dataBaseAddressPath,
+            driver:sqlite3.Database
+        })
+        expressAppInstance.listen(3000, ()=>{
+            console.log(`Database Connection object received in the Variable "databaseConnectionObject"
+Server started listening at http://localhost:3000/`)
+        })
+    }catch(e){
+        console.log(`Database Error ${e.message}`)
+        process.exit(1)
+    }
+}
+
+initializeDBAndServerAtPort3000();
